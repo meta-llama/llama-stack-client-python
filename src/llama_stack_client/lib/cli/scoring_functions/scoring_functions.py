50f6012c5d25caab688644deb67a979ba936072a
Dinesh Yeduguru <dineshyv@fb.com>
2024-11-13 11:50:25 -0800

    scoring functions register

--- src/llama_stack_client/lib/cli/scoring_functions/scoring_functions.py
+++ src/llama_stack_client/lib/cli/scoring_functions/scoring_functions.py
@@ -5,6 +5,9 @@
 # the root directory of this source tree.
 
 import click
+import yaml
+from typing import Optional
+import json
 
 from .list import list_scoring_functions
 
@@ -15,5 +18,44 @@ def scoring_functions():
     pass
 
 
+@scoring_functions.command()
+@click.option("--scoring-fn-id", required=True, help="Id of the scoring function")
+@click.option("--description", required=True, help="Description of the scoring function")
+@click.option("--return-type", required=True, help="Return type of the scoring function")
+@click.option("--provider-id", help="Provider ID for the scoring function", default=None)
+@click.option("--provider-scoring-fn-id", help="Provider's scoring function ID", default=None)
+@click.option("--params", type=str, help="Parameters for the scoring function in JSON format")
+@click.pass_context
+def register(
+    ctx,
+    scoring_fn_id: str,
+    description: str,
+    return_type: str,
+    provider_id: Optional[str],
+    provider_scoring_fn_id: Optional[str],
+    params: Optional[str],
+):
+    """Register a new scoring function"""
+    client = ctx.obj["client"]
+
+    if params:
+        try:
+            params = json.loads(params)
+        except json.JSONDecodeError:
+            raise click.BadParameter("Parameters must be valid JSON")
+
+    response = client.scoring_functions.register(
+        scoring_fn_id=scoring_fn_id,
+        description=description,
+        return_type=return_type,
+        provider_id=provider_id,
+        provider_scoring_fn_id=provider_scoring_fn_id,
+        params=params,
+    )
+    if response:
+        click.echo(yaml.dump(response.dict()))
+
+
 # Register subcommands
 scoring_functions.add_command(list_scoring_functions)
+scoring_functions.add_command(register)
