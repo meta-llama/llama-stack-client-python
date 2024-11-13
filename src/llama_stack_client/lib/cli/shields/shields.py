afbb25d0e34e1e70311828649b8e45696c1934f4
Dinesh Yeduguru <dineshyv@fb.com>
2024-11-13 11:41:40 -0800

    shield register

--- src/llama_stack_client/lib/cli/shields/shields.py
+++ src/llama_stack_client/lib/cli/shields/shields.py
@@ -5,8 +5,9 @@
 # the root directory of this source tree.
 
 import click
-
-from .list import list_shields
+from typing import Optional
+import yaml
+from llama_stack_client.lib.cli.common.utils import print_table_from_response
 
 
 @click.group()
@@ -15,5 +16,47 @@ def shields():
     pass
 
 
+@click.command("list")
+@click.pass_context
+def list(ctx):
+    """Show available safety shields on distribution endpoint"""
+    client = ctx.obj["client"]
+
+    shields_list_response = client.shields.list()
+    headers = []
+    if shields_list_response and len(shields_list_response) > 0:
+        headers = sorted(shields_list_response[0].__dict__.keys())
+
+    if shields_list_response:
+        print_table_from_response(shields_list_response, headers)
+
+
+@shields.command()
+@click.option("--shield-id", required=True, help="Id of the shield")
+@click.option("--provider-id", help="Provider ID for the shield", default=None)
+@click.option("--provider-shield-id", help="Provider's shield ID", default=None)
+@click.option("--params", type=str, help="JSON configuration parameters for the shield", default=None)
+@click.pass_context
+def register(
+    ctx,
+    shield_id: str,
+    provider_id: Optional[str],
+    provider_shield_id: Optional[str],
+    params: Optional[str],
+):
+    """Register a new safety shield"""
+    client = ctx.obj["client"]
+
+    response = client.shields.register(
+        shield_id=shield_id,
+        params=params,
+        provider_id=provider_id,
+        provider_shield_id=provider_shield_id,
+    )
+    if response:
+        click.echo(yaml.dump(response.dict()))
+
+
 # Register subcommands
-shields.add_command(list_shields)
+shields.add_command(list)
+shields.add_command(register)
