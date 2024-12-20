4744e32bf87a6597e7504b0f7b1e28fe322142ee
Zhonglin Han <hanzlfs@users.noreply.github.com>
2024-12-20 11:29:36 -0800

    Update _client.py

--- src/llama_stack_client/_client.py
+++ src/llama_stack_client/_client.py
@@ -1,7 +1,7 @@
 # File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
 
-import json
 from __future__ import annotations
+import json
 
 import os
 from typing import Any, Union, Mapping
