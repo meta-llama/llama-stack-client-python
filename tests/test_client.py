b18416caebd1351629f1f9ccc2c31946117f6702
Xi Yan <xiyan@meta.com>
2025-01-09 13:02:07 -0800

    test_client

--- tests/test_client.py
+++ tests/test_client.py
@@ -1629,10 +1629,8 @@ class TestAsyncLlamaStackClient:
         import asyncio
         import nest_asyncio
         import threading
-
-        from llama_stack_client._utils import asyncify
         from llama_stack_client._base_client import get_platform
-
+        from llama_stack_client._utils import asyncify
 
         async def test_main() -> None:
             result = await asyncify(get_platform)()
