ae3a040d25164b97fa5c586e911b8b151b6b67e2
Vladimir Ivic <vivic@meta.com>
2025-01-31 11:50:47 -0800

    Sync updates from stainless branch: main

--- src/llama_stack_client/types/list_eval_tasks_response.py
+++ src/llama_stack_client/types/list_eval_tasks_response.py
@@ -1,12 +1,11 @@
 # File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
 
-from typing import List
 
 from .._models import BaseModel
-from .eval_task import EvalTask
+from .eval_task_list_response import EvalTaskListResponse
 
 __all__ = ["ListEvalTasksResponse"]
 
 
 class ListEvalTasksResponse(BaseModel):
-    data: List[EvalTask]
+    data: EvalTaskListResponse
