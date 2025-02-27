b9f7b5803a0813f6f9ac886008280d2332b7f1df
Xi Yan <xiyan@meta.com>
2025-02-26 17:38:30 -0800

    async agent wip

--- src/llama_stack_client/lib/agents/agent.py
+++ src/llama_stack_client/lib/agents/agent.py
@@ -3,17 +3,15 @@
 #
 # This source code is licensed under the terms described in the LICENSE file in
 # the root directory of this source tree.
-from typing import Iterator, List, Optional, Tuple, Union
+from typing import AsyncIterator, Iterator, List, Optional, Tuple, Union
 
-from llama_stack_client import LlamaStackClient
+from llama_stack_client import AsyncLlamaStackClient, LlamaStackClient
 
 from llama_stack_client.types import ToolResponseMessage, UserMessage
 from llama_stack_client.types.agent_create_params import AgentConfig
 from llama_stack_client.types.agents.turn import CompletionMessage, Turn
 from llama_stack_client.types.agents.turn_create_params import Document, Toolgroup
-from llama_stack_client.types.agents.turn_create_response import (
-    AgentTurnResponseStreamChunk,
-)
+from llama_stack_client.types.agents.turn_create_response import AgentTurnResponseStreamChunk
 from llama_stack_client.types.shared.tool_call import ToolCall
 
 from .client_tool import ClientTool
@@ -22,7 +20,28 @@ from .tool_parser import ToolParser
 DEFAULT_MAX_ITER = 10
 
 
-class Agent:
+class AgentMixin:
+    def _get_tool_calls(self, chunk: AgentTurnResponseStreamChunk) -> List[ToolCall]:
+        if chunk.event.payload.event_type not in {"turn_complete", "turn_awaiting_input"}:
+            return []
+
+        message = chunk.event.payload.turn.output_message
+        if message.stop_reason == "out_of_tokens":
+            return []
+
+        if self.tool_parser:
+            return self.tool_parser.get_tool_calls(message)
+
+        return message.tool_calls
+
+    def _get_turn_id(self, chunk: AgentTurnResponseStreamChunk) -> Optional[str]:
+        if chunk.event.payload.event_type not in ["turn_complete", "turn_awaiting_input"]:
+            return None
+
+        return chunk.event.payload.turn.turn_id
+
+
+class Agent(AgentMixin):
     def __init__(
         self,
         client: LlamaStackClient,
@@ -56,25 +75,6 @@ class Agent:
         self.session_id = agentic_system_create_session_response.session_id
         self.sessions.append(self.session_id)
         return self.session_id
-
-    def _get_tool_calls(self, chunk: AgentTurnResponseStreamChunk) -> List[ToolCall]:
-        if chunk.event.payload.event_type not in {"turn_complete", "turn_awaiting_input"}:
-            return []
-
-        message = chunk.event.payload.turn.output_message
-        if message.stop_reason == "out_of_tokens":
-            return []
-
-        if self.tool_parser:
-            return self.tool_parser.get_tool_calls(message)
-
-        return message.tool_calls
-
-    def _get_turn_id(self, chunk: AgentTurnResponseStreamChunk) -> Optional[str]:
-        if chunk.event.payload.event_type not in ["turn_complete", "turn_awaiting_input"]:
-            return None
-
-        return chunk.event.payload.turn.turn_id
 
     def _run_tool(self, tool_calls: List[ToolCall]) -> ToolResponseMessage:
         assert len(tool_calls) == 1, "Only one tool call is supported"
@@ -189,3 +189,71 @@ class Agent:
 
             if n_iter >= max_iter:
                 raise Exception(f"Turn did not complete in {max_iter} iterations")
+
+
+class AsyncAgent:
+    def __init__(
+        self,
+        client: AsyncLlamaStackClient,
+        agent_config: AgentConfig,
+        client_tools: Tuple[ClientTool] = (),
+        tool_parser: Optional[ToolParser] = None,
+    ):
+        self.client = client
+        self.agent_config = agent_config
+        self.client_tools = {t.get_name(): t for t in client_tools}
+        self.sessions = []
+        self.tool_parser = tool_parser
+        self.builtin_tools = {}
+
+    async def initialize(self) -> None:
+        agentic_system_create_response = await self.client.agents.create(
+            agent_config=self.agent_config,
+        )
+        self.agent_id = agentic_system_create_response.agent_id
+        for tg in self.agent_config["toolgroups"]:
+            for tool in await self.client.tools.list(toolgroup_id=tg):
+                self.builtin_tools[tool.identifier] = tool
+
+    async def create_session(self, session_name: str) -> str:
+        agentic_system_create_session_response = await self.client.agents.session.create(
+            agent_id=self.agent_id,
+            session_name=session_name,
+        )
+        self.session_id = agentic_system_create_session_response.session_id
+        self.sessions.append(self.session_id)
+        return self.session_id
+
+    async def create_turn(
+        self,
+        messages: List[Union[UserMessage, ToolResponseMessage]],
+        session_id: Optional[str] = None,
+        toolgroups: Optional[List[Toolgroup]] = None,
+        documents: Optional[List[Document]] = None,
+        stream: bool = True,
+    ) -> AsyncIterator[AgentTurnResponseStreamChunk] | Turn:
+        if stream:
+            return self._create_turn_streaming(messages, session_id, toolgroups, documents)
+        else:
+            chunks = [x async for x in self._create_turn_streaming(messages, session_id, toolgroups, documents)]
+            if not chunks:
+                raise Exception("Turn did not complete")
+            return chunks[-1].event.payload.turn
+
+    async def _create_turn_streaming(
+        self,
+        messages: List[Union[UserMessage, ToolResponseMessage]],
+        session_id: Optional[str] = None,
+        toolgroups: Optional[List[Toolgroup]] = None,
+        documents: Optional[List[Document]] = None,
+    ) -> AsyncIterator[AgentTurnResponseStreamChunk]:
+        turn_response = await self.client.agents.turn.create(
+            agent_id=self.agent_id,
+            session_id=session_id or self.session_id[-1],
+            messages=messages,
+            stream=True,
+            documents=documents,
+        )
+
+        async for chunk in turn_response:
+            yield chunk
