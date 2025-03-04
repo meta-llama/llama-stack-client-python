cd3b2b815437905f01c3a819f9e162a023f98043
Eric Huang <erichuang@meta.com>
2025-03-04 14:39:50 -0800

    chore: use rich to format logs
    Summary:
    
    Test Plan:

--- src/llama_stack_client/_utils/_logs.py
+++ src/llama_stack_client/_utils/_logs.py
@@ -1,15 +1,16 @@
 import os
 import logging
+from rich.logging import RichHandler
 
 logger: logging.Logger = logging.getLogger("llama_stack_client")
 httpx_logger: logging.Logger = logging.getLogger("httpx")
 
 
 def _basic_config() -> None:
-    # e.g. [2023-10-05 14:12:26 - llama_stack_client._base_client:818 - DEBUG] HTTP Request: POST http://127.0.0.1:4010/foo/bar "200 OK"
     logging.basicConfig(
-        format="[%(asctime)s - %(name)s:%(lineno)d - %(levelname)s] %(message)s",
-        datefmt="%Y-%m-%d %H:%M:%S",
+        format="%(message)s",
+        datefmt="[%X]",
+        handlers=[RichHandler(rich_tracebacks=True)],
     )
 
 
