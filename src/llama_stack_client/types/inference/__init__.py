4888d8ad39bf78f590cff1e73489811ab9a074d1
Xi Yan <xiyan@meta.com>
2025-03-12 16:00:37 -0700

    Sync updates from stainless branch: main

--- src/llama_stack_client/types/inference/__init__.py
+++
@@ -1,6 +0,0 @@
-# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
-
-from __future__ import annotations
-
-from .embeddings import Embeddings as Embeddings
-from .embedding_create_params import EmbeddingCreateParams as EmbeddingCreateParams
