b664564fe1c4771a7872286d0c2ac96c47816939
Xi Yan <xiyan@meta.com>
2025-03-17 16:55:57 -0700

    feat: datasets api updates (#203)
    # What does this PR do?
    - sync SDK
    
    [//]: # (If resolving an issue, uncomment and update the line below)
    [//]: # (Closes #[issue-number])
    
    ## Test Plan
    - See https://github.com/meta-llama/llama-stack/pull/1573
    
    
    [//]: # (## Documentation)
    [//]: # (- [ ] Added a Changelog entry if the change is significant)

--- src/llama_stack_client/types/dataset_register_params.py
+++ src/llama_stack_client/types/dataset_register_params.py
@@ -3,26 +3,65 @@
 from __future__ import annotations
 
 from typing import Dict, Union, Iterable
-from typing_extensions import Required, TypedDict
+from typing_extensions import Literal, Required, TypeAlias, TypedDict
 
-from .shared_params.param_type import ParamType
-
-__all__ = ["DatasetRegisterParams", "URL"]
+__all__ = ["DatasetRegisterParams", "Source", "SourceUriDataSource", "SourceRowsDataSource"]
 
 
 class DatasetRegisterParams(TypedDict, total=False):
-    dataset_id: Required[str]
+    purpose: Required[Literal["post-training/messages", "eval/question-answer", "eval/messages-answer"]]
+    """The purpose of the dataset.
 
-    dataset_schema: Required[Dict[str, ParamType]]
+    One of - "post-training/messages": The dataset contains a messages column with
+    list of messages for post-training. { "messages": [ {"role": "user", "content":
+    "Hello, world!"}, {"role": "assistant", "content": "Hello, world!"}, ] } -
+    "eval/question-answer": The dataset contains a question column and an answer
+    column for evaluation. { "question": "What is the capital of France?", "answer":
+    "Paris" } - "eval/messages-answer": The dataset contains a messages column with
+    list of messages and an answer column for evaluation. { "messages": [ {"role":
+    "user", "content": "Hello, my name is John Doe."}, {"role": "assistant",
+    "content": "Hello, John Doe. How can I help you today?"}, {"role": "user",
+    "content": "What's my name?"}, ], "answer": "John Doe" }
+    """
 
-    url: Required[URL]
+    source: Required[Source]
+    """The data source of the dataset.
+
+    Examples: - { "type": "uri", "uri": "https://mywebsite.com/mydata.jsonl" } - {
+    "type": "uri", "uri": "lsfs://mydata.jsonl" } - { "type": "uri", "uri":
+    "data:csv;base64,{base64_content}" } - { "type": "uri", "uri":
+    "huggingface://llamastack/simpleqa?split=train" } - { "type": "rows", "rows": [
+    { "messages": [ {"role": "user", "content": "Hello, world!"}, {"role":
+    "assistant", "content": "Hello, world!"}, ] } ] }
+    """
+
+    dataset_id: str
+    """The ID of the dataset. If not provided, an ID will be generated."""
 
     metadata: Dict[str, Union[bool, float, str, Iterable[object], object, None]]
-
-    provider_dataset_id: str
-
-    provider_id: str
+    """The metadata for the dataset. - E.g. {"description": "My dataset"}"""
 
 
-class URL(TypedDict, total=False):
+class SourceUriDataSource(TypedDict, total=False):
+    type: Required[Literal["uri"]]
+
     uri: Required[str]
+    """The dataset can be obtained from a URI.
+
+    E.g. - "https://mywebsite.com/mydata.jsonl" - "lsfs://mydata.jsonl" -
+    "data:csv;base64,{base64_content}"
+    """
+
+
+class SourceRowsDataSource(TypedDict, total=False):
+    rows: Required[Iterable[Dict[str, Union[bool, float, str, Iterable[object], object, None]]]]
+    """The dataset is stored in rows.
+
+    E.g. - [ {"messages": [{"role": "user", "content": "Hello, world!"}, {"role":
+    "assistant", "content": "Hello, world!"}]} ]
+    """
+
+    type: Required[Literal["rows"]]
+
+
+Source: TypeAlias = Union[SourceUriDataSource, SourceRowsDataSource]
