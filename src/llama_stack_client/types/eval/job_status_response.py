827130646316cf0f242f404f1e518822f08f852e
Xi Yan <xiyan@meta.com>
2025-03-21 13:35:12 -0700

    Sync updates from stainless branch: yanxi0830/dev

--- src/llama_stack_client/types/eval/job_status_response.py
+++
@@ -1,7 +0,0 @@
-# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
-
-from typing_extensions import Literal, TypeAlias
-
-__all__ = ["JobStatusResponse"]
-
-JobStatusResponse: TypeAlias = Literal["completed", "in_progress", "failed", "scheduled"]
