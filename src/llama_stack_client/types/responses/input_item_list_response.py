6520401532715af793e5fdf9295c6ebbd8116cca
Hardik Shah <hjshah@meta.com>
2025-06-11 17:39:43 -0700

    Sync updates from stainless branch: hardikjshah/dev

---
+++ src/llama_stack_client/types/responses/input_item_list_response.py
@@ -0,0 +1,111 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import List, Union, Optional
+from typing_extensions import Literal, Annotated, TypeAlias
+
+from ..._utils import PropertyInfo
+from ..._models import BaseModel
+
+__all__ = [
+    "InputItemListResponse",
+    "Data",
+    "DataOpenAIResponseOutputMessageWebSearchToolCall",
+    "DataOpenAIResponseOutputMessageFunctionToolCall",
+    "DataOpenAIResponseInputFunctionToolCallOutput",
+    "DataOpenAIResponseMessage",
+    "DataOpenAIResponseMessageContentUnionMember1",
+    "DataOpenAIResponseMessageContentUnionMember1OpenAIResponseInputMessageContentText",
+    "DataOpenAIResponseMessageContentUnionMember1OpenAIResponseInputMessageContentImage",
+    "DataOpenAIResponseMessageContentUnionMember2",
+]
+
+
+class DataOpenAIResponseOutputMessageWebSearchToolCall(BaseModel):
+    id: str
+
+    status: str
+
+    type: Literal["web_search_call"]
+
+
+class DataOpenAIResponseOutputMessageFunctionToolCall(BaseModel):
+    arguments: str
+
+    call_id: str
+
+    name: str
+
+    type: Literal["function_call"]
+
+    id: Optional[str] = None
+
+    status: Optional[str] = None
+
+
+class DataOpenAIResponseInputFunctionToolCallOutput(BaseModel):
+    call_id: str
+
+    output: str
+
+    type: Literal["function_call_output"]
+
+    id: Optional[str] = None
+
+    status: Optional[str] = None
+
+
+class DataOpenAIResponseMessageContentUnionMember1OpenAIResponseInputMessageContentText(BaseModel):
+    text: str
+
+    type: Literal["input_text"]
+
+
+class DataOpenAIResponseMessageContentUnionMember1OpenAIResponseInputMessageContentImage(BaseModel):
+    detail: Literal["low", "high", "auto"]
+
+    type: Literal["input_image"]
+
+    image_url: Optional[str] = None
+
+
+DataOpenAIResponseMessageContentUnionMember1: TypeAlias = Annotated[
+    Union[
+        DataOpenAIResponseMessageContentUnionMember1OpenAIResponseInputMessageContentText,
+        DataOpenAIResponseMessageContentUnionMember1OpenAIResponseInputMessageContentImage,
+    ],
+    PropertyInfo(discriminator="type"),
+]
+
+
+class DataOpenAIResponseMessageContentUnionMember2(BaseModel):
+    text: str
+
+    type: Literal["output_text"]
+
+
+class DataOpenAIResponseMessage(BaseModel):
+    content: Union[
+        str, List[DataOpenAIResponseMessageContentUnionMember1], List[DataOpenAIResponseMessageContentUnionMember2]
+    ]
+
+    role: Literal["system", "developer", "user", "assistant"]
+
+    type: Literal["message"]
+
+    id: Optional[str] = None
+
+    status: Optional[str] = None
+
+
+Data: TypeAlias = Union[
+    DataOpenAIResponseOutputMessageWebSearchToolCall,
+    DataOpenAIResponseOutputMessageFunctionToolCall,
+    DataOpenAIResponseInputFunctionToolCallOutput,
+    DataOpenAIResponseMessage,
+]
+
+
+class InputItemListResponse(BaseModel):
+    data: List[Data]
+
+    object: Literal["list"]
